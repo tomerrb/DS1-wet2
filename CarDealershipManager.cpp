c41eabe76bae37e13366039540864d0c60559dcb
spektorroma <76950538+spektorroma@users.noreply.github.com>
2021-06-12 19:32:19 +0300

    Add files via upload

---
+++ CarDealershipManager.cpp
@@ -0,0 +1,34 @@
+
+#include "CarDealershipManager.h"
+
+#include <stdlib.h>
+#define MAXSIZE 10
+
+CarDealershipManager::CarDealershipManager() :
+	max_size(MAXSIZE),
+	current_size(0)
+{
+	agency = (Union**)malloc(sizeof(Union*) * MAXSIZE);
+
+}
+
+CarDealershipManager::~CarDealershipManager()
+	
+{
+	free(agency);
+}
+
+StatusType CarDealershipManager::AddAgency() {
+	if (current_size == max_size) {
+		max_size = max_size * 2;
+		agency = (Union**)realloc(agency, sizeof(Union*) * max_size);
+		if (!agency)
+			return ALLOCATION_ERROR;
+
+	}
+	Union* new_agency = new Union();
+	agency[current_size] = new_agency;
+	current_size++;
+	return SUCCESS;
+}
+
---
+++ Union.h
@@ -0,0 +1,16 @@
+#ifndef UNION_H
+#define UNION_H
+
+
+
+
+class Union
+{
+public:
+
+	Union();
+	~Union();
+
+};
+
+#endif
