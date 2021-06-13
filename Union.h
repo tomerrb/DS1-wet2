c30c562d2a32e350c43c22c2d03bfac534392be1
spektorroma <76950538+spektorroma@users.noreply.github.com>
2021-06-13 13:34:35 +0300

    Update Union.h

--- Union.h
+++ Union.h
@@ -1,16 +1,59 @@
+#include "tree.h"
+
 #ifndef UNION_H
 #define UNION_H
 
+int compare_int_keys(int key1, int key2) {
+	return key2 - key1;
+}
 
+int compare_double_keys(doubleInt key1, doubleInt key2) {
+	if (key1.get_score() == key2.get_score())
+		return key2.get_number() - key1.get_number();
+	return key2.get_score() - key1.get_score();
+}
 
-
-class Union
-{
+class doubleInt {
+	int score;
+	int number;
 public:
+	doubleInt(int score, int number) :
+		score(score),
+		number(number) {
 
-	Union();
-	~Union();
+	}
+	~doubleInt() = default;
+
+	int get_score() {
+		return score;
+	}
+	int get_number() {
+		return number;
+	}
 
 };
+
+namespace Union {
+	class Agency
+	{
+		Tree<int, int> score_tree;
+		Tree<doubleInt, doubleInt> final_tree;
+		Agency* parent;
+		int rank;
+
+
+	public:
+
+		Agency();
+		~Agency();
+		Agency* find();
+		void change_parent(Agency* agency);
+		bool SellCar(int typeID, int k);
+		int GetIthSoldType(int i);
+
+	};
+
+	bool UniteAgencies(const Agency& agency1, const Agency& agency2);
+}
 
 #endif
