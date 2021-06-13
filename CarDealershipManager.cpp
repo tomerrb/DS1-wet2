d242b3183ab9264c436ae15df49f68ad4b7d0514
spektorroma <76950538+spektorroma@users.noreply.github.com>
2021-06-13 13:33:30 +0300

    Update CarDealershipManager.cpp

--- CarDealershipManager.cpp
+++ CarDealershipManager.cpp
@@ -1,4 +1,3 @@
-
 #include "CarDealershipManager.h"
 
 #include <stdlib.h>
@@ -8,8 +7,7 @@ CarDealershipManager::CarDealershipManager() :
 	max_size(MAXSIZE),
 	current_size(0)
 {
-	agency = (Union**)malloc(sizeof(Union*) * MAXSIZE);
-
+	agency = (Union::Agency**)malloc(sizeof(Union::Agency*) * MAXSIZE);
 }
 
 CarDealershipManager::~CarDealershipManager()
@@ -21,14 +19,23 @@ CarDealershipManager::~CarDealershipManager()
 StatusType CarDealershipManager::AddAgency() {
 	if (current_size == max_size) {
 		max_size = max_size * 2;
-		agency = (Union**)realloc(agency, sizeof(Union*) * max_size);
+		agency = (Union::Agency**)realloc(agency, sizeof(Union::Agency*) * max_size);
 		if (!agency)
 			return ALLOCATION_ERROR;
 
 	}
-	Union* new_agency = new Union();
+	Union::Agency* new_agency = new Union::Agency();
 	agency[current_size] = new_agency;
 	current_size++;
 	return SUCCESS;
 }
+
+StatusType CarDealershipManager::SellCar(int agencyID, int typeID, int k) {
+	if (agencyID >= current_size)
+		return FAILURE;
+	agency[agencyID]->SellCar(typeID, k);
+	return SUCCESS;
+}
+
+
 
